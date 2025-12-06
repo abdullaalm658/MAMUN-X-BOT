9b3627b4e9154b05a6210d737895df4c71bee818
abdullaalm658 <mamunranam896@gmail.com>
2025-12-06 23:14:10 +0600

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝗠𝗔𝗠𝗨𝗡 ✘ 𝗕𝗢𝗧',
+  author: '‎',
+  botName: "𝗠𝗔𝗠𝗨𝗡 ✘ 𝗕𝗢𝗧",
+  botOwner: '𝙋𝙞𝙣𝙜', // Your name
+  ownerNumber: '8801786269298', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
